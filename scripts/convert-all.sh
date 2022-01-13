5da8c52798a889f1a0b457f97a94e3998048a2ce
Jeff Palm <jeff@jeffpalm.com>
2022-01-13 06:56:13 -0500

    don't resize images

--- scripts/convert-all.sh
+++ scripts/convert-all.sh
@@ -3,5 +3,5 @@
 for f in data/in/*; do
     out=data/out/$(basename $f)
     echo "Covert $f -> $out"
-    go run main.go --input "$f" --output "$out" --resize_width=300 --resize_height=300
+    go run main.go --input "$f" --output "$out"
 done
