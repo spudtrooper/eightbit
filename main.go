f7f360fc0d19fa21e3d3bad3f0181a162689ce09
Jeff Palm <jeff@jeffpalm.com>
2022-01-13 06:54:08 -0500

    update Thu Jan 13 06:54:08 EST 2022

--- main.go
+++ main.go
@@ -5,29 +5,33 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/spudtrooper/eightbit/eightbit"
+	"github.com/spudtrooper/eightbit/gitversion"
 	"github.com/spudtrooper/goutil/check"
 )
 
 var (
-	input     = flag.String("input", "", "input image")
-	output    = flag.String("output", "", "output image")
-	blockSize = flag.Int("block_size", 16, "blocksize for downsampling")
+	input        = flag.String("input", "", "input image")
+	output       = flag.String("output", "", "output image")
+	blockSize    = flag.Int("block_size", 16, "blocksize for downsampling")
 	resizeHeight = flag.Int("resize_height", 0, "height in pixels of the final image; must be used with --resize_width")
-	resizeWidth = flag.Int("resize_width", 0, "width in pixels of the final image; must be used with --resize_height")
+	resizeWidth  = flag.Int("resize_width", 0, "width in pixels of the final image; must be used with --resize_height")
 )
 
 func realMain() error {
+	if gitversion.CheckVersionFlag() {
+		return nil
+	}
 	if *input == "" {
 		return errors.Errorf("--input required")
 	}
 	if *output == "" {
 		return errors.Errorf("--output required")
 	}
-	if err := eightbit.Convert(*input, *output, 
-		eightbit.ConvertBlockSize(*blockSize), 
-		eightbit.ConvertBlockSize(*blockSize), 
+	if err := eightbit.Convert(*input, *output,
 		eightbit.ConvertBlockSize(*blockSize),
-		); err != nil {
+		eightbit.ConvertResizeWidth(uint(*resizeHeight)),
+		eightbit.ConvertResizeHeight(uint(*resizeWidth)),
+	); err != nil {
 		return err
 	}
 	return nil
